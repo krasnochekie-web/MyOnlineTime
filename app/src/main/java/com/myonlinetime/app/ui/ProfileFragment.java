c3930221c47f302e58f80986d66cb7bbd3c51888
krasnochekie-web <krasnochekie@gmail.com>
2026-05-05 12:24:00 +0300

    Update ProfileFragment.java

--- app/src/main/java/com/myonlinetime/app/ui/ProfileFragment.java
+++ app/src/main/java/com/myonlinetime/app/ui/ProfileFragment.java
@@ -19,7 +19,6 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
@@ -386,12 +385,6 @@ public class ProfileFragment extends Fragment {
                 if (fetchProfileDataRunnable != null) fetchProfileDataRunnable.run();
                 refreshCounts(activity);
                 activity.updateGlobalBackground(true); 
-            } else {
-                new Handler(Looper.getMainLooper()).postDelayed(() -> {
-                    if (isAdded() && isHidden() && activity.navigator != null && activity.navigator.getCurrentTabIndex() != 4) {
-                        activity.updateGlobalBackground(false);
-                    }
-                }, 750);
             }
         }
     }
