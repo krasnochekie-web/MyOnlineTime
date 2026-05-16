f1107eef05e2f8bb62313312569e5207e8cd0ca3
krasnochekie-web <krasnochekie@gmail.com>
2026-05-16 19:21:47 +0300

    Create MyAppGlideModule.java

---
+++ app/src/main/java/com/myonlinetime/app/utils/MyAppGlideModule.java
@@ -0,0 +1,9 @@
+package com.myonlinetime.app.utils;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {
+    // Оставь класс пустым, Glide сам сгенерирует всё необходимое при сборке
+}
